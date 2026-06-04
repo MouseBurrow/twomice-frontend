cd548553f0ef3717fed9b7f73b74a3b0e908955d
Sylviromi <sylviromi.ss@proton.me>
2026-06-05 02:26:32 +0300

    chore: remove OldPostRedirect, /nib/ is the only canonical path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes, useParams } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth.tsx";
 import Home from "./pages/Home";
 import Board from "./pages/Board";
@@ -9,11 +9,6 @@ import CreatePost from "./pages/CreatePost";
 import Layout from "./components/layout/Layout";
 import "./assets/App.scss";
 
-function OldPostRedirect() {
-    const { board, post } = useParams<{ board: string; post: string }>();
-    return <Navigate to={`/b/${board}/nib/${post}`} replace />;
-}
-
 export default function App() {
     return (
         <Routes>
@@ -22,7 +17,6 @@ export default function App() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/b/:board" element={<Board/>}/>
                 <Route path="/b/:board/nib/:post" element={<Post/>}/>
-                <Route path="/b/:board/post/:post" element={<OldPostRedirect/>}/>
                 <Route path="/b/:board/new" element={<CreatePost/>}/>
                 <Route path="/settings" element={<Settings/>}/>
                 <Route path="/profile" element={<Profile/>}/>

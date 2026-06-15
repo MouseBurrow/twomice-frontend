acbde30cfaacc54e732be57ee40c1df369aa4e06
Sylviromi <sylviromi.ss@proton.me>
2026-06-15 17:05:56 +0300

    refactor: remove unused components and dead code
    Delete components that were replaced or are no longer needed:
    PostGrid, BoardGrid, FollowedBoardsBar, Header, AdminGuard,
    MobileNav, Logo, VoteColumn.

--- src/components/home/Header.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomeBanner() {
-    return (
-        <div className="home-banner">
-            <h1 className="home-banner-title">🐭 Mischief Board</h1>
-            <p className="home-banner-sub">Plots, schemes, and small disasters</p>
-        </div>
-    );
-}
